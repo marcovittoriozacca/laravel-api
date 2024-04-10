1891931663bc59c82031d64c0485e5c2495fb103
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-10 19:08:29 +0200

    feat: Lead controller + validation and response

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request){
 
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            "firstname" => ['required', 'max:150',],
+            "surname" => ['required', 'max:150',],
+            "email" => ['required', 'email'],
+            "content" => ['required'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                "success" => false,
+                "errors" => $validator->errors(),
+            ]);
+        }
+
+        Lead::create($data);
     }
 }
